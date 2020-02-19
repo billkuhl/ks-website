b5427481fcfb839067edaabf1a74724e290a07f5
Daniel Gonzalez <dggsax@gmail.com>
2020-02-19 02:02:35 -0500

    Updated summer housing, added 404 handler

--- web/__init__.py
+++ web/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -8,5 +8,9 @@ from werkzeug import url_encode
 import web.config as config
 
 app.config['APP_NAME'] = config.APP_NAME
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404 
 
 import web.controllers
